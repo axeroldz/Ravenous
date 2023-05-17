9d4cf5a5f2af6e9d2fa0ec36dc6ce87656b1695d
David.K <davidkwan1276@gmail.com>
2023-05-17 12:21:31 +0900

    change back

--- src/utils/YelpApi.js
+++ src/utils/YelpApi.js
@@ -1,10 +1,9 @@
 const apiKey = "GaZk4y5R4lV-xx2VZmfEDly7igTlJ5uwehsGOE0QDCxfTStdEoIImohg_TEiWeFQNh40Hg9cv4RIImVhoaoGbhOivmyHVf_he476cSvxccyBQ1Tsz5p_qaeKZzJeZHYx";
-const baseURL = 'https://api.yelp.com/v3/businesses/search?'
+const baseURL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?'
 const options = {
     headers : {
         Authorization: `Bearer ${apiKey}`
-    },
-    mode: 'no-cors'
+    }
 }
 const yelpApi = (term,loc,sortBy) =>{
         return fetch(`${baseURL}term=${term}&location=${loc}&sort_by=${sortBy}`,options)
